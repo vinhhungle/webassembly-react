921707fd34df301d4668f98c86dfaa31b75dfb36
Hung Le <vinhhung.le@gmail.com>
2021-01-06 15:57:29 +0700

    update lib use f32

--- src/rust-wasm/src/lib.rs
+++ src/rust-wasm/src/lib.rs
@@ -4,15 +4,15 @@ use wasm_bindgen::prelude::*;
 use pulldown_cmark::{Parser, Options, html};
 
 #[wasm_bindgen]
-pub fn add(a: f64, b: f64) -> f64 {
+pub fn add(a: f32, b: f32) -> f32 {
   a + b
 }
 
 #[wasm_bindgen]
-pub fn fibonacci(n: f64) -> f64 {
-  let mut a: f64 = 0.0;
-  let mut b: f64 = 1.0;
-  for _ in 0..n.round() as i64 {
+pub fn fibonacci(n: f32) -> f32 {
+  let mut a: f32 = 0.0;
+  let mut b: f32 = 1.0;
+  for _ in 0..n.round() as i32 {
       let tmp = b;
       b += a;
       a = tmp;
@@ -21,7 +21,7 @@ pub fn fibonacci(n: f64) -> f64 {
 }
 
 #[wasm_bindgen]
-pub fn bm_fibonacci(n: f64, m: f64) {
+pub fn bm_fibonacci(n: f32, m: f32) {
   for _ in 0..m.round() as i64 {
     fibonacci(n);
   }
@@ -40,4 +40,16 @@ pub fn render_md(markdown_input: &str) -> String {
   html::push_html(&mut html_output, parser);
 
   return html_output;
+}
+
+pub fn run() {
+  println!("====== lib ======");
+
+  let markdown_input = "Hello world, this is a ~~complicated~~ *very simple* example.";
+  let expected_html = "<p>Hello world, this is a <del>complicated</del> <em>very simple</em> example.</p>\n";
+
+  let html_output = render_md(markdown_input);
+  assert_eq!(expected_html, &html_output);
+  
+  println!("Fib: {}", fibonacci(45.0));
 }
